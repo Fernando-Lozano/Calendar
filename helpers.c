eba4ffc401a6f6b5a792dd2c297ff74e5cbc0387
Fernando <60452941+Fernando-Lozano@users.noreply.github.com>
2020-10-11 15:57:32 -0500

    Improved style

--- helpers.c
+++ helpers.c
@@ -59,11 +59,11 @@ void printCalendar(char *month, int year, int startDay, int days)
 {
      // print header of calendar
     printf("\nCalendar of %i\n\n", year);
+
     for (int i = 0; i < (33 - strlen(month)) / 2; i++)
     {
         printf("-");
-    }
-    
+    } 
     month[0] = toupper(month[0]);
     printf("%s", month);
     // print remainder of stars depending on string length of month
